2522d354257ca0e358cb3565a2bb598e1a0b6519
tijn <tijnjh@proton.me>
2025-03-14 00:15:27 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const router = express.Router();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "pages")));
+
+const dirs = await fs.readdir("src/pages");
+
+const pages = [];
+
+await Promise.all(
+  dirs.map(async (dir) => {
+    const dirPath = path.join(__dirname, `/pages/${dir}`);
+    const stats = await fs.stat(dirPath);
+
+    if (stats.isDirectory()) {
+      const content = await fs.readdir(dirPath);
+      if (content.includes("index.html")) {
+        pages.push(dir);
+      }
+    }
+  }),
+);
+
+router.get("/", (_, res) => {
+  res.render("list", { pages: pages });
+});
+
+app.use("/", router);
+
+app.listen(4200);
---
+++ src/pages/conventional-commits/app.js
@@ -0,0 +1,128 @@
+const elType = document.getElementById("type");
+const elScope = document.getElementById("scope");
+const elGitmoji = document.getElementById("gitmoji");
+const elGitmojis = document.getElementById("gitmojis");
+const elDescription = document.getElementById("description");
+const elOutput = document.getElementById("output");
+
+const elToaster = document.getElementById("toaster");
+
+const commitTypes = Object.freeze([
+  {
+    type: "feat",
+    category: "Features",
+    description: "A new feature",
+  },
+  {
+    type: "fix",
+    category: "Bug Fixes",
+    description: "A bug fix",
+  },
+  {
+    type: "docs",
+    category: "Documentation",
+    description: "Documentation only changes",
+  },
+  {
+    type: "style",
+    category: "Styles",
+    description:
+      "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc.)",
+  },
+  {
+    type: "refactor",
+    category: "Code Refactoring",
+    description: "A code change that neither fixes a bug nor adds a feature",
+  },
+  {
+    type: "perf",
+    category: "Performance Improvements",
+    description: "A code change that improves performance",
+  },
+  {
+    type: "test",
+    category: "Tests",
+    description: "Adding missing tests or correcting existing tests",
+  },
+  {
+    type: "build",
+    category: "Build System",
+    description:
+      "Changes that affect the build system or external dependencies",
+  },
+  {
+    type: "ci",
+    category: "Continuous Integration",
+    description: "Changes to our CI configuration files and scripts",
+  },
+  {
+    type: "chore",
+    category: "Chores",
+    description: "Other changes that don't modify src or test files",
+  },
+  {
+    type: "revert",
+    category: "Reverts",
+    description: "Reverts a previous commit",
+  },
+]);
+
+commitTypes.forEach(({ type, category, description }) => {
+  const elTypeOption = document.createElement("option");
+  elTypeOption.textContent = `${type}: ${category} (${description})`;
+  elTypeOption.value = type;
+  elType.append(elTypeOption);
+});
+
+async function fetchGitmoji() {
+  const url =
+    "https://raw.githubusercontent.com/carloscuesta/gitmoji/1e96bea76777bdc9a99842d61b013d990efd2d17/packages/gitmojis/src/gitmojis.json";
+  const response = await (await fetch(url)).json();
+  return response;
+}
+
+fetchGitmoji().then((r) => {
+  const gitmojis = r.gitmojis;
+  gitmojis.forEach(({ emoji, description }) => {
+    const elGitmojiOption = document.createElement("option");
+    elGitmojiOption.textContent = `${description}`;
+    elGitmojiOption.value = emoji;
+    elGitmojis.append(elGitmojiOption);
+  });
+});
+
+function updateOutput() {
+  const type = elType.value;
+  const scope = elScope.value;
+  const gitmoji = elGitmoji.value;
+  const description = elDescription.value;
+
+  elOutput.textContent = `${type}${
+    !!scope ? `(${scope})` : ""
+  }: ${gitmoji} ${description}`;
+}
+
+elType.oninput = updateOutput;
+elScope.oninput = updateOutput;
+elGitmoji.oninput = updateOutput;
+elDescription.oninput = updateOutput;
+
+elOutput.onclick = () => {
+  navigator.clipboard
+    .writeText(elOutput.textContent)
+    .then(() => toast("📋 Successfully copied commit message to clipboard"))
+    .catch((error) => toast(`❌ Failed to copy to clipboard: ${error}`));
+};
+
+function toast(message) {
+  const elToast = document.createElement("div");
+  elToast.className = "toast";
+  elToast.textContent = message;
+  elToaster.append(elToast);
+
+  setTimeout(() => {
+    elToast.classList.add("animate-out");
+    elToast.ontransitionend = () => elToast.remove();
+    return;
+  }, 2000);
+}
