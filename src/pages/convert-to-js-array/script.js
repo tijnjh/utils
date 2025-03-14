b31c63d8dd2aa955251e0b0c12e6c6e703ef0d52
tijn <tijn@noreply.codeberg.org>
2025-03-14 10:36:41 +0000

    Add src/pages/convert-to-js-array/script.js

---
+++ src/pages/convert-to-js-array/script.js
@@ -0,0 +1,36 @@
+(function () {
+  var elInput, elOutput;
+  elInput = document.getElementById("input");
+  elOutput = document.getElementById("output");
+  elInput.oninput = function (e) {
+    return (elOutput.innerHTML = (function (it) {
+      return (elOutput.innerHTML = it);
+    })(
+      (function (it) {
+        return it.join("<br>");
+      })(
+        (function (it) {
+          return it.map(function (it) {
+            return '"' + it + '",';
+          });
+        })(
+          (function (it) {
+            return it.filter(function (it) {
+              return !!it;
+            });
+          })(
+            (function (it) {
+              return it.map(function (it) {
+                return it.trim();
+              });
+            })(
+              (function (it) {
+                return it.split("\n");
+              })(e.target.value)
+            )
+          )
+        )
+      )
+    ));
+  };
+}).call(this);
